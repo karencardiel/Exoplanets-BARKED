8a700308b70903a968fb8363128188ccf505a9a9
karencardiel <karencardielo2@gmail.com>
2025-10-22 02:35:13 -0600

    port

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def predict():
         return jsonify({'error': f'An error occurred during prediction: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
